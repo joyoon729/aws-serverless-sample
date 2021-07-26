68b2c74720c5ccf3e9b15bc3d887c5768bd800c9
YoonGi Jo <joyoon729@gmail.com>
2021-07-27 07:17:43 +0900

    fix config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_VsW8DzYWD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '8ld5d22ai26d3du2ei2opfer5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '18ld5d22ai26d3du2ei2opfer5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
